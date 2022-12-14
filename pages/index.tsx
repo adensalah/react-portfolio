62d2310b59deadc69c6b5058bde46b6c2386ad49
Adhan Moge Salah <56362845+adensalah@users.noreply.github.com>
2022-12-14 17:02:48 +0300

    Headers

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,9 +45,8 @@ export default function Home() {
           </nav>
           <div className="text-center p-10">
             <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">Adhan Moge</h2>
-            <h3 className="text-2xl py-2 md:text-3xl">Developer and Designer.</h3>
-            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto">Freelancer provinding service for programming.. 
-
+            <h3 className="text-2xl py-2 md:text-3xl">Developer & Designer.</h3>
+            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto">
             </p>
           </div>
           <div className="text-5xl flex justify-center gap-16 py-3 text-gray-60">
@@ -59,8 +58,6 @@ export default function Home() {
             <Image src={deved} layout="fill" objectFit="cover" alt={""}/>
           </div>
         </section>
-
-
         <section>
           <div>
             <h3 className="text-3xl py-1">Services i offer</h3>
